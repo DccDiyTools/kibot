833d5484c7a6865dc9d6be1cec7e6149862a3ee4
Daniel Vilas Perulan <daniel.vilas@gmail.com>
2025-01-04 20:00:41 +0100

    update

--- repo_files/kibot_shell.sh
+++ repo_files/kibot_shell.sh
@@ -6,8 +6,15 @@ else
   git -C .kibot pull
 fi
 
+LOCAL_LIBS=""
+if [ -d /Applications/KiCad/KiCad.app/Contents/SharedSupport ]; then
+  LOCAL_LIBS="-v /Applications/KiCad/KiCad.app/Contents/SharedSupport:/usr/share/kicad/"
+fi
+
 docker run -it --rm \
   -v $(pwd):/work \
-  -v $(pwd)/.kibot:/opt/kibot
+  -v $(pwd)/.kibot:/opt/kibot \
+  $LOCAL_LIBS \
   -w /work \
+  -e KIBOT_ROOT_SCRIPTS=/opt/kibot/kibot \
   ghcr.io/inti-cmnb/kicad8_auto_full:latest 
--- repo_files/run_kibot.sh
+++ repo_files/run_kibot.sh
@@ -1,32 +1,23 @@
 #!/bin/bash
-KIBOT_DIR=$(git rev-parse --show-toplevel)
-RES=$?
-if [ $RES -eq 0 ] 
-then
-    KIBOT_DIR=$KIBOT_DIR/kibot
-else
-    KIBOT_DIR=/home/$USER/workdir/kibot
-fi    
-#echo $KIBOT_DIR
+
 export PCBDRAW_LIB_PATH=$KIBOT_DIR/../
 rm -rf doc
 CURRENT=`pwd`
 
-#Check current DirName
-BOARD=`basename $CURRENT`
-if [ ! -f  $BOARD.kicad_sch ]
-then
-    BOARD=$(dirname "$CURRENT")
-    BOARD=`basename $BOARD`
-fi
-#If not found, check with parent Dir name
-if [ ! -f  $BOARD.kicad_sch ]
-then   
-    echo Expected file not found: $BOARD.kicad_sch
-    #exit 0
+
+PRO_FILES=("*.kicad_pro")
+#echo $PRO_FILES
+if [ ${#PRO_FILES[@]} == 1 ];then
+    BOARD=$(echo $PRO_FILES)
+    BOARD="${BOARD%.*}"
+else 
+    echo "Too many ${#PRO_FILES[@]} kicad_pro files. "
+    exit 0
     #If not found change manually.
-    BOARD=CHANGE_ME
+    #BOARD=CHANGE_ME
 fi
+
+#FILES="00_erc_drc.kibot.yaml 01_pdf_doc.kibot.yaml 02_build_doc.kibot.yaml 03_graphic_doc.kibot.yaml 04_fab_board.kibot.yaml 05_fab_pcba_jlcpcb.kibot"
 FILES="00_erc_drc.kibot.yaml 01_pdf_doc.kibot.yaml 02_build_doc.kibot.yaml 03_graphic_doc.kibot.yaml 04_fab_board.kibot.yaml"  
 
 #FILES="02_build_doc.kibot.yaml"  
@@ -34,13 +25,14 @@ FILES="00_erc_drc.kibot.yaml 01_pdf_doc.kibot.yaml 02_build_doc.kibot.yaml 03_gr
 
 for i in $FILES 
 do
-    echo "Running $i"
-    kibot -e $BOARD.kicad_sch -b $BOARD.kicad_pcb -d doc -c $KIBOT_DIR/$i $@
+    echo "Running $i on $BOARD"
+    kibot -e $BOARD.kicad_sch -b $BOARD.kicad_pcb -d doc -c $KIBOT_ROOT_SCRIPTS/$i $@
 done 
 
 if [ -f manual.kibot.yaml ]; 
 then
-kibot -e $BOARD.kicad_sch -b $BOARD.kicad_pcb -d manual -c manual.kibot.yaml $@
+    echo "Running manual.kibot.yaml on $BOARD"
+    kibot -e $BOARD.kicad_sch -b $BOARD.kicad_pcb -d manual -c manual.kibot.yaml $@
 fi
 
 rm -f doc/*.ogv
